02f9180f19d085c79996401a701a0cb65cb2803b
kyubuns <mail@kyubuns.net>
2013-12-28 21:33:43 +0900

    usersテーブル綺麗にした

--- server/db/migrate/001_create_users.rb
+++ server/db/migrate/001_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string :uid
+      t.string :name
       t.string :provider
+      t.string :provider_id
       t.string :role
       t.timestamps
     end
