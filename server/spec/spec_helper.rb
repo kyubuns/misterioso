99e406df807094933cfc596407c150e58e0e4a46
kyubuns <mail@kyubuns.net>
2013-12-28 23:37:24 +0900

    database_cleaner導入

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -1,19 +1,23 @@
 PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+require 'database_cleaner'
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
+
+  conf.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  conf.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  conf.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
-# You can use this method to custom specify a Rack app
-# you want rack-test to invoke:
-#
-#   app Server::App
-#   app Server::App.tap { |a| }
-#   app(Server::App) do
-#     set :foo, :bar
-#   end
-#
 def app(app = nil, &blk)
   @app ||= block_given? ? app.instance_eval(&blk) : app
   @app ||= Padrino.application
