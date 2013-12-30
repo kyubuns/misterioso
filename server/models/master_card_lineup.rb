3b0b928fcc200203f02ce3e21b1d81c827cc19e0
kyubuns <mail@kyubuns.net>
2013-12-31 04:14:29 +0900

    実装が思いつかない

--- server/models/master_card_lineup.rb
+++ server/models/master_card_lineup.rb
@@ -1,7 +1,16 @@
 class MasterCardLineup < ActiveRecord::Base
 
-  validates :code,        presence: true, uniqueness: { scope: :card_code }
+  validates :name,        presence: true, uniqueness: { scope: :card_code }
   validates :card_code,   presence: true, numericality: true
   validates :probability, presence: true, numericality: true
 
+  def self.get(name)
+    Random.rand get_total_probability(name)
+    #???????????
+  end
+
+  private
+  def self.get_total_probability(name)
+    MasterCardLineup.where('name = ?', name).sum(:probability)
+  end
 end
--- server/spec/models/master_card_lineup_spec.rb
+++ server/spec/models/master_card_lineup_spec.rb
@@ -1,4 +1,40 @@
 require 'spec_helper'
 
 describe MasterCardLineup do
+  describe "#get_random" do
+      before(:each) {
+        MasterCardLineup.create(name: 'normal_gacha', card_code: 1, probability: 10)
+        MasterCardLineup.create(name: 'normal_gacha', card_code: 2, probability: 10)
+        MasterCardLineup.create(name: 'normal_gacha', card_code: 3, probability: 10)
+        MasterCardLineup.create(name: 'normal_gacha', card_code: 4, probability: 10)
+        MasterCardLineup.create(name: 'normal_gacha', card_code: 5, probability: 10)
+        MasterCardLineup.create(name: 'rare_gacha', card_code: 100, probability: 10)
+        MasterCardLineup.create(name: 'rare_gacha', card_code: 101, probability: 10)
+        MasterCardLineup.create(name: 'rare_gacha', card_code: 102, probability: 10)
+        MasterCardLineup.create(name: 'rare_gacha', card_code: 103, probability: 10)
+        MasterCardLineup.create(name: 'rare_gacha', card_code: 104, probability: 1)
+      }
+
+      it "get normal_gacha total_probability" do
+        MasterCardLineup.send(:get_total_probability, 'normal_gacha').should == 50
+      end
+
+      it "get rare_gacha total_probability" do
+        MasterCardLineup.send(:get_total_probability, 'rare_gacha').should == 41
+      end
+
+      it "get normal_gacha" do
+        10.times do
+          card_code = MasterCardLineup.get('normal_gacha')
+          card_code.should be_between(1, 5)
+        end
+      end
+
+      it "get rare_gacha" do
+        10.times do
+          card_code = MasterCardLineup.get('rare_gacha')
+          card_code.should be_between(100, 104)
+        end
+      end
+  end
 end
