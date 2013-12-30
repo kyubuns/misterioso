9b2df8da434430234f4de19ee6a36ca9630999a2
kyubuns <mail@kyubuns.net>
2013-12-31 00:46:18 +0900

    レアリティのvalidete追加

--- server/models/master_card.rb
+++ server/models/master_card.rb
@@ -1,6 +1,7 @@
 class MasterCard < ActiveRecord::Base
 
-  validates  :code, presence: true
-  validates  :name, presence: true
+  validates  :code,   presence: true
+  validates  :name,   presence: true
+  validates  :rarity, presence: true, numericality: true, inclusion: { in: 1..5 }
 
 end
--- server/spec/factories/master_cards.rb
+++ server/spec/factories/master_cards.rb
@@ -3,6 +3,7 @@ require 'forgery'
 FactoryGirl.define do
   factory :master_card do
     sequence(:code)
-    name Forgery::Name.full_name
+    name   Forgery::Name.full_name
+    rarity Forgery::Basic.number(at_least: 1, at_most: 5)
   end
 end
