b985c9682bf23bfa7f3b7bf43913cc267d18413a
kyubuns <mail@kyubuns.net>
2013-12-31 06:19:31 +0900

    migrate失敗したので消しておく

--- server/db/migrate/013_calc_power_to_all_characters.rb
+++ server/db/migrate/013_calc_power_to_all_characters.rb
@@ -1,8 +1,2 @@
 class CalcPowerToAllCharacters < ActiveRecord::Migration
-  def self.up
-    Character.all.each do |character|
-      character.calc_power
-      character.save!
-    end
-  end
 end
