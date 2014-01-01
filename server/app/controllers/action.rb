2d69859907735381bbe7fe10b99d2adae3e0e857
kyubuns <mail@kyubuns.net>
2014-01-01 22:47:21 +0900

    controllerのID間違ってたのをこっそり修正

--- server/app/controllers/action.rb
+++ server/app/controllers/action.rb
@@ -60,7 +60,7 @@ Server::App.controllers :action do
     redirect url("/mypage")
   end
 
-  get :equip, :map => "/purge", :protect => true do
+  get :purge, :map => "/purge", :protect => true do
     begin
       current_account.character.purge
     rescue OperationError
